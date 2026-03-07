eaa7f09c447a52bacf2c2bb471d63a808cb95125
Biktu <robinsonwelkinbiktuchumpi@gmail.com>
2026-03-07 15:06:54 -0500

    fix: mejoras de diseño Family Love

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import HeroSection from "./_components/HeroSection/hero-section";
-import BookingSection from "./_components/BookingSection/booking-section";
 import OfficeSection from "./_components/OfficeSection/office-section";
 import AboutUsSection from "./_components/AboutUsSection/section";
 
